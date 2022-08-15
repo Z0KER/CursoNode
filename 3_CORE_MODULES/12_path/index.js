bece4af202de45a8f845b8c1327af5d201b0f9a6
zoker <flaviogabriel2603@gmail.com>
2022-08-15 20:36:38 -0300

    módulo path

---
+++ 3_CORE_MODULES/12_path/index.js
@@ -0,0 +1,7 @@
+const path = require('path')
+
+const customPath = '/relatorios/flavio/relatorio.pdf'
+
+console.log(path.dirname(customPath))
+console.log(path.basename(customPath))
+console.log(path.extname(customPath))
